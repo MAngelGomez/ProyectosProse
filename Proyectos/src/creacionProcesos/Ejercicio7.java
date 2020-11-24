59987521f59ba70ef811bdf472e0d873fe6e9e6f
miguel <miguel@DESKTOP-41S3ID2>
2020-11-24 21:14:03 +0100

    Ejercicio7añadido

---
+++ Proyectos/src/creacionProcesos/Ejercicio7.java
@@ -0,0 +1,22 @@
+package creacionProcesos;
+
+import java.io.IOException;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+
+		
+		String comandoLinux = "firefox iescristobaldemonroy.es";
+		String comandoWindows = "C:\\Program Files\\Mozilla Firefox\\firefox.exe iescristobaldemonroy.es";
+		
+		String comando = comandoWindows;
+		ProcessBuilder pb = new ProcessBuilder(comando);
+		try {
+			pb.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
