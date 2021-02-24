3e6d7ae82961ae306049b6b419975a3e526fc066
miguel <miguel@DESKTOP-41S3ID2>
2021-02-24 20:24:54 +0100

    repaso

--- Proyectos/src/ManagerCalculadora1.java
+++ Proyectos/src/ManagerCalculadora1.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -53,16 +54,16 @@ public class ManagerCalculadora1 {
 	
 	public void sumar(int a, int b) {
 		try {
-			URL url= new URL(protocoloWeb+urlOperacion+"?API_KEY="+apiKey+"&TIPO_OPERACION=SUMA&SUMANDO_A=" +a+ "&SUMANDO_B=" + b);
-			URLConnection conexion=url.openConnection();
-			
 			protocoloWeb="https://psp2021site.000webhostapp.com/";
 			apiKey=112;
 			urlOperacion="realizaOperacion.php";
 			
-			BufferedReader br;
+			URL url= new URL(protocoloWeb+urlOperacion+"?API_KEY="+apiKey+"&TIPO_OPERACION=SUMA&SUMANDO_A=" +a+ "&SUMANDO_B=" + b);
+			URLConnection conexion=url.openConnection();
+			
+			
 			InputStream is=conexion.getInputStream();
-			br=new BufferedReader(new InputStreamReader(is));
+			BufferedReader br=new BufferedReader(new InputStreamReader(is));
 			
 			String linea;
 			while((linea=br.readLine())!=null) {
@@ -89,16 +90,22 @@ public class ManagerCalculadora1 {
 	
 	public void restar(int a, int b) {
 		try {
-			URL url= new URL(protocoloWeb+urlOperacion+"?API_KEY="+apiKey+"\"&TIPO_OPERACION=RESTA&MINUENDO=\"" +a+ "&SUSTRAENDO=" + b);
-			URLConnection conexion=url.openConnection();
 			
 			protocoloWeb="https://psp2021site.000webhostapp.com/";
 			apiKey=112;
 			urlOperacion="realizaOperacion.php";
 			
-			BufferedReader br;
-			InputStream is=conexion.getInputStream();
-			br=new BufferedReader(new InputStreamReader(is));
+			URL url= new URL(protocoloWeb+urlOperacion);
+			URLConnection conexion=url.openConnection();
+			
+			String valores="?API_KEY="+apiKey+"\"&TIPO_OPERACION=RESTA&MINUENDO=\"" +a+ "&SUSTRAENDO=" + b;
+			
+			PrintWriter output=new PrintWriter(conexion.getOutputStream());
+			
+			output.write(valores);
+			output.close();
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(conexion.getInputStream()));
 			
 			String linea;
 			while((linea=br.readLine())!=null) {
@@ -116,16 +123,17 @@ public class ManagerCalculadora1 {
 	}
 	public void multiplicar(int a, int b) {
 		try {
-			URL url= new URL(protocoloWeb+urlOperacion+"?API_KEY="+apiKey+"&TIPO_OPERACION=MULTIPLICACION&OPERADOR_1=" +a+ "&OPERADOR_2=" + b);
-			URLConnection conexion=url.openConnection();
-			
+					
 			protocoloWeb="https://psp2021site.000webhostapp.com/";
 			apiKey=112;
 			urlOperacion="realizaOperacion.php";
 			
-			BufferedReader br;
+			URL url= new URL(protocoloWeb+urlOperacion+"?API_KEY="+apiKey+"&TIPO_OPERACION=MULTIPLICACION&OPERADOR_1=" +a+ "&OPERADOR_2=" + b);
+			URLConnection conexion=url.openConnection();
+			
+			
 			InputStream is=conexion.getInputStream();
-			br=new BufferedReader(new InputStreamReader(is));
+			BufferedReader br=new BufferedReader(new InputStreamReader(is));
 			
 			String linea;
 			while((linea=br.readLine())!=null) {
